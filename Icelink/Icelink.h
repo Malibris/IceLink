8807a1a0f2fa101f4cc3f48e115b311fea891b5d
Robert Abramczyk <robert.abramczyk@netguru.pl>
2018-02-17 21:20:22 +0100

    Remove headers

--- Icelink/Icelink.h
+++ Icelink/Icelink.h
@@ -15,19 +15,11 @@ FOUNDATION_EXPORT const unsigned char IcelinkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Icelink/PublicHeader.h>
 
-#import <Icelink/FM-Swift3.h>
 #import <Icelink/FM.h>
-#import <Icelink/FMIceLink-Swift3.h>
 #import <Icelink/FMIceLink.h>
-#import <Icelink/FMIceLinkSimple-Swift3.h>
 #import <Icelink/FMIceLinkSimple.h>
-#import <Icelink/FMIceLinkWebRTC-Swift3.h>
 #import <Icelink/FMIceLinkWebRTC.h>
-#import <Icelink/FMIceLinkWebSync-Swift3.h>
 #import <Icelink/FMIceLinkWebSync.h>
-#import <Icelink/FMWebSync-Swift3.h>
 #import <Icelink/FMWebSync.h>
-#import <Icelink/FMWebSyncChat-Swift3.h>
 #import <Icelink/FMWebSyncChat.h>
-#import <Icelink/FMWebSyncSubscribers-Swift3.h>
 #import <Icelink/FMWebSyncSubscribers.h>
